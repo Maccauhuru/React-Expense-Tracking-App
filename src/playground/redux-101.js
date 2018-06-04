38d55ad90ac524ba9e85ab8cba91468649d52e4d
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-04 17:07:17 -0500

    new action generators created : refactoring

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,31 +2,55 @@ import {
   createStore
 } from "redux";
 
+
+const incrementCount = ({incrementBy = 1} = {})=>({
+  type : 'INCREMENT',
+  incrementBy 
+}
+);
+
+const decreaseCount = ({decrementBy = 1} = {})=>({
+  type : 'DECREMENT',
+  decrementBy
+}
+);
+const resetCount = () =>({
+  type : 'RESET'
+});
+
+const setCount = ({count})=>({ 
+  type  : 'SET',
+  count 
+});
+
+//action generators - functions that return action Objects
 const store = createStore((state = {
   count: 0
 }, action) => {
   
   switch (action.type) {
-    
     case "INCREMENT":
-    const incrementBy = typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case "DECREMENT":
-    const decreaseby = typeof action.decreaseby==="number" ? action.decreaseby : 1
       return {
-        count: state.count - decreaseby
+        count: state.count - action.decrementBy
       };
       case "MULTIPLYBYTEN":
     const multiply10 = typeof action.multiply10==="number" ? action.multiply10 : 1
       return {
         count: state.count * multiply10
       };
+    case "SET":
+    return{
+      count : action.count
+    }
     case "RESET":
       return {
         count: 0
       };
+    
     default:
       return state;
   }
@@ -51,28 +75,23 @@ store.dispatch({
   incrementBy : 7
 });
 
-store.dispatch({
-  type: "DECREMENT",
-  decreaseby : 20
-});
-
-store.dispatch({
-  type: "DECREMENT"
-});
-
+store.dispatch(incrementCount({incrementBy:20}));
+store.dispatch(incrementCount({incrementBy:200}));
+store.dispatch(resetCount());
+store.dispatch(decreaseCount({decrementBy:100}));
 store.dispatch({
   type:"MULTIPLYBYTEN",
   multiply10 : 10
 })
+
+store.dispatch(setCount({count:100}));
 
 unsubscribe();
 
 store.dispatch({
   type: "DECREMENT"
 });
-store.dispatch({
-  type: "RESET"
-});
+
 store.dispatch({
   type: "INCREMENT"
 });
