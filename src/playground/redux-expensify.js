1de929ca37c81fb5ce9da4c5d8449cf7257aec98
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-09 19:46:32 -0500

    combined reducers added

---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,14 @@
+import {
+  createStore,
+  combineReducers
+} from 'redux';
+
+const demoState = {
+  expenses: [{
+    id: 'wjafnianfpifreij',
+    description: 'July Rental Expenses',
+    notes: 'some notes',
+    amount: 455678,
+    createdAt: 0
+  }]
+};
