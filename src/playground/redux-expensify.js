84a814dff26bf4c7fec34f8f5aceb83bfd4441e3
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-22 20:22:17 -0500

    msort by startDate,endDate and Text filters added

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -129,6 +129,14 @@ switch(action.type){
 }
 }
 
+{ /*GET INVISIBLE EXPENSES*/ }
+const getVisibleExpenses=(expenses,{text,sortBy,startDate,endDate})=>{
+  return expenses.filter((expense)=>{
+  const startDateMatch = typeof startDate!=='number' || expense.createdAt >= startDate;
+  const endDateMatch = typeof endDate !=='number' || expense.createdAt <= endDate;;
+  const textMatch = typeof text =='string' && expense.description.toLowerCase().includes(text.toLowerCase());
+  return startDateMatch && endDateMatch && textMatch;
+  })};
 
 
 { /*STORE CREATION*/ }
@@ -138,24 +146,26 @@ const store = createStore(combineReducers({
 }));
 
 store.subscribe(()=>{
-console.info(store.getState());
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
+console.info(visibleExpenses);
 }
 )
 
-// const expenseOne = store.dispatch(addExpense({description:'Rent',amount: 100}));
-// const expenseTwo = store.dispatch(addExpense({ description: 'Food', amount: 500 }));
+ const expenseOne = store.dispatch(addExpense({description:'Rent',amount: 100 , createdAt:1000}));
+ const expenseTwo = store.dispatch(addExpense({ description: 'Food', amount: 500 , createdAt:-1000 }));
 
 
 // store.dispatch(removeExpense({id : expenseOne.expense.id}));
 // store.dispatch(editExpense(expenseTwo.expense.id,{amount:550}));
-// store.dispatch(setTextFilter('rent'));
+//store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter(''));
 // store.dispatch(sortByAmount('455678'));
 // store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(125));
-store.dispatch(setStartDate());
-store.dispatch(setEndDate(1250));
+ //store.dispatch(setStartDate(0));
+store.dispatch(setTextFilter('RENT'));
+ //store.dispatch(setEndDate(1250));
 
 
 const demoState = {
