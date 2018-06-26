2b3b598ae487e8268b011ad7cb33781820243b4e
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-26 17:20:17 -0500

    refactor App for better organization and manageability

---
+++ src/reducers/filters.js
@@ -0,0 +1,44 @@
+{ /*FILTERS REDUCER*/ }
+const filtersReducerDefaultStates = {
+text : '',
+sortBy : 'date',
+startDate : undefined,
+endDate : undefined
+}
+const filtersReducer = (state = filtersReducerDefaultStates,action)=>{
+switch(action.type){
+  case 'SET_TEXT_FILTER' :
+  return {
+    ...state,
+    text: action.text
+  }
+  case 'SORT_BY_AMOUNT' :{
+    return{
+      ...state,
+      sortBy: 'amount'
+    }
+  }
+  case 'SORT_BY_DATE' : {
+    return{
+      ...state,
+      sortBy: action.startDate
+    }
+  }
+  case 'SET_START_DATE' :{
+    return {
+      ...state,
+      startDate : action.startDate
+    }
+  }
+  case 'SET_END_DATE':
+    {
+      return {
+        ...state,
+        endDate: action.endDate
+      }
+    }
+  default : return state;
+}
+}
+
+export default filtersReducer;
