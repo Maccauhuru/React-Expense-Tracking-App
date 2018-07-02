a9be8b73d5e57f4f0d6f34c6d67f9dee127e2572
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-02 15:05:16 -0500

    new filters for expense amount and date

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -4,11 +4,16 @@ import { setTextFilter ,sortByAmount , sortByDate} from '../actions/filters';
 
 const ExpenseListFilters =(props)=>(
     <div>
-    <input type='text'  value={props.filters.text} onChange={(e)=>{
+    <input type='text'
+    value={props.filters.text}
+    onChange={(e)=>{
      props.dispatch(setTextFilter(e.target.value));
     }}/>
-    <select type='text' value={props.filters.text} onChange={(e)=>{e.target.value =='date'?
-    props.dispatch(sortByDate(e.target.value)) : props.dispatch(sortByAmount(e.target.value))}}>
+
+    <select type='text'
+    value={props.filters.sortByDate} 
+    onChange={(e)=>{e.target.value ==='date'?
+    props.dispatch(sortByDate()) : props.dispatch(sortByAmount())}}>
     <option value="date" >Date</option>
     <option value="amount">Amount</option>
     </select>
