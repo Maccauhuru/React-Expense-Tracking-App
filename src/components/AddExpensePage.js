7c2b0787ace433b690d69a0207c20db7cc920842
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-02 15:03:52 -0500

    add expensepage component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import ExpenseForm from './ExpenseForm';
 
-const AddExpensePage = () => <div>Lets CREATE some expenses</div>;
+const AddExpensePage = () => {
+return(<div>
+<h4>Lets CREATE some expenses</h4>
+<ExpenseForm />
+</div>);
+}
 
 export default AddExpensePage;
