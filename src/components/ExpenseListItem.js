55de210e26b00a7ff9fc5e1166939acc0c922244
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-08 15:40:05 -0500

    new Link added to h3 descriptions so that expenses are editable

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { removeExpense } from '../actions/expenses';
 import { connect } from 'react-redux';
 
 const ExpenseListItem = ({dispatch , id ,description , amount , createdAt}) => {
   return (
     <div>
-      <h3>{description}</h3>
+      <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
       <p>Amount :{amount} - Created :{createdAt}</p>
       <button  onClick={()=>dispatch(removeExpense({id}))}>REMOVE</button>
     </div>
