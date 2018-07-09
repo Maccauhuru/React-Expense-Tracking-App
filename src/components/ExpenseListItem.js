0215fa49e119b0b326e2b23ba8ed4603c9c927a9
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-09 17:38:26 -0500

    removeExpense removed from this page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { removeExpense } from '../actions/expenses';
-import { connect } from 'react-redux';
+
 
 const ExpenseListItem = ({dispatch , id ,description , amount , createdAt}) => {
   return (
     <div>
       <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
       <p>Amount :{amount} - Created :{createdAt}</p>
-      <button  onClick={()=>dispatch(removeExpense({id}))}>REMOVE</button>
     </div>
   )
 }
@@ -19,4 +17,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect()(ExpenseListItem);
+export default (ExpenseListItem);
