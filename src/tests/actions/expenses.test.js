e8fe2755cec5ef64d2b73ba3c295862bc7fcc7a1
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-13 21:43:31 -0500

    test cases for start and end date filters

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -21,3 +21,39 @@ expect(result).toEqual({
         }
 });
 });
+
+test('should setup addExpense action object with provided values',()=>{
+const expenseData = {
+    description:'Renovations',
+    notes:'Major house renovations',
+    amount:1000000,
+    createdAt:1531531300539
+}
+const action = addExpense(expenseData);
+expect(action).toEqual({
+type :'ADD_EXPENSE',
+expense :{
+    ...expenseData,
+    id : expect.any(String)
+}
+})
+});
+
+
+test("should setup addExpense action object with default values", () => {
+    const expenseData = {
+            description :'',
+            notes : '',
+            amount : 0,
+            createdAt : 0
+    }
+
+const action = addExpense();
+expect(action).toEqual({
+    type : 'ADD_EXPENSE',
+    expense :{
+       ...expenseData,
+       id : expect.any(String)
+    }
+})
+});
