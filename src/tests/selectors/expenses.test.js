73be2c2666bb34943c57faa9c468dac75634d3b2
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-18 18:28:09 -0500

    expenses test cases added

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -53,7 +53,7 @@ const filters = {
 text : '',
 sortBy : 0,
 startDate : undefined,
-endDate : moment(0)
+endDate : moment(0).add(2 , 'days')
 } 
 const result = selectExpenses(expenses,filters);
 expect(result).toEqual([expenses[0],expenses[1]]);    
@@ -64,12 +64,12 @@ expect(result).toEqual([expenses[0],expenses[1]]);
 test('should filter by date',()=>{
 const filters={
 text : '',
-sortBy : 0,
+sortBy : 'date',
 startDate : undefined,
 endDate : undefined 
 }
 const result = selectExpenses(expenses,filters);
-expect(result).toEqual([expenses[0],expenses[1],expenses[2]]);
+expect(result).toEqual([expenses[2],expenses[0],expenses[1]]);
 });
 
 //should sort by amount
@@ -78,9 +78,9 @@ test('should filter by amount',()=>{
 const filters={
 text : '',
 sortBy : 'amount',
-startDate : moment(0),
+startDate : undefined,
 endDate : undefined 
 }
 const result = selectExpenses(expenses,filters);
-expect(result).toEqual([expenses[2], expenses[1], expenses[0]]);
+expect(result).toEqual([expenses[0], expenses[2], expenses[1]]);
 });
