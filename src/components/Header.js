50fb5a8ca561b64497da62698c343f074da39cde
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-25 15:17:34 -0500

    Removed help navlink

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,9 +13,6 @@ const Header = () => (
     <NavLink to="/create" activeClassName="is-active">
       Create
     </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
   </div>
 );
 
