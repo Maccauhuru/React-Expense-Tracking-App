7f4809911473720b0bedb486c745c9feb3e70cbc
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-26 16:39:25 -0500

    new test cases : component snapshot testing

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
 
 test('should render heading correctly',()=>{
-const renderer = new ReactShallowRenderer;
-renderer.render(<Header />);
-expect(renderer.getRenderOutput()).toMatchSnapshot();
+const wrapper = shallow(<Header />);
+expect(wrapper).toMatchSnapshot();
 });
