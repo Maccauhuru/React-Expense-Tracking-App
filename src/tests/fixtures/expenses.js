596acb6202d5c71e9bc975f4a91b1476923739b0
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-08-02 19:03:41 -0500

    update mock fixtures

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -2,21 +2,21 @@ import moment from 'moment';
 export default [{
         id: 1,
         description: 'Turntable',
-        notes: '',
+        note: '',
         amount: 14500,
         createdAt: 0
     },
     {
         id: 2,
         description: 'Sandwich',
-        notes: '',
+        note: '',
         amount: 299,
         createdAt: moment(0).subtract(4, 'days').valueOf()
     },
     {
         id: 3,
         description: 'Insurance',
-        notes: '',
+        note: '',
         amount: 12200,
         createdAt: moment(0).add(4, 'days').valueOf()
     },
