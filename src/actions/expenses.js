0165425e6afe879fd6431b9ca661bd75ed6f0406
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-08-30 18:34:30 -0500

    modified actions and refactored

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,38 +1,33 @@
 import uuid from 'uuid';
-{ /*CREATE ACTION GENERATORS*/ }
-//ADD_EXPENSE
- export const addExpense = ({
+
+// ADD_EXPENSE
+export const addExpense = (
+  {
     description = '',
-    notes = '',
+    note = '',
     amount = 0,
     createdAt = 0
-} = {}) => ({
-    type: 'ADD_EXPENSE',
-    expense: {
-        id: uuid(),
-        description,
-        notes,
-        amount,
-        createdAt
-    }
-})
-
-//REMOVE_EXPENSE
-export const removeExpense = ({
-    id
-} = {}) => ({
-    type: 'REMOVE_EXPENSE',
-    id
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
 });
 
-//EDIT_EXPENSE
+// REMOVE_EXPENSE
+export const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+// EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
-    type: 'EDIT_EXPENSE',
-    id,
-    updates
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
 });
-
-
-
-
-
