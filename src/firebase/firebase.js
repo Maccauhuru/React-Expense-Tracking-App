2ce0f26ece769086e691ae7afd5f1bd41f92b2af
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2019-01-02 16:36:28 -0600

    Promises Updates
    Changes :
    *updated firebase db using set & update
    *added some new objects to db using push
    
    Why:
    * Learning set() , update() , ref() and other methods from docs
    *  Arrays do not work as expected in Firebase therefore we need to solely use objects with auto generated key
    
    These changes addresses the need by:
    
    * Author - www.github.com/maccauhuru

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,19 +10,95 @@ const config = {
 firebase.initializeApp(config);
 const db = firebase.database();
 
-db.ref().set({
-artist: 'Sean Paul',
-riddim : "Bubble Up",
-year : 2005,
-available : true,
-stores : ['Apple Store','Google Play','Basecamp'],
-released : {
-    songname : 'legalize it',
-    country: 'Jamaica',
-    recorded : 2005,
-    price : '$10.00'
-}
+db.ref('expenses').push({
+    description :'Food',
+    note : '',
+    amount:12500,
+    createdAt:12081982
 });
-//Change Firebase DB values
-// db.ref('released/price').set('$15.75');
-// db.ref('released/country').set('Kingston,Jamaica');
+db.ref('expenses').push({
+    description :'Clothes',
+    note : '',
+    amount:1070,
+    createdAt:14071998
+});
+db.ref('expenses').push({
+    description :'Medicine',
+    note : '',
+    amount:89010,
+    createdAt:18031991
+});
+
+// db.ref('notes').push({
+//     title: 'All eyes on me',
+//     author: 'Tupac Shakur'
+// });
+
+//Retrieve Snapshot from DB
+// const onValueChange = db.ref().on('value',(snapshot)=>{
+//     const data = snapshot.val();
+//     console.log(`${data.artist} is currently living in ${data.released.country}`);
+// },(err)=>{
+// console.error('Snapshot failed to retrieve', err)
+// });
+
+
+
+//Fetch all the data
+// db.ref('artist').once('value').then((snapshot)=>{
+// const data = snapshot.val();
+// console.log('data fetched successsfully :' ,data);
+// }).catch((err)=>{
+// console.log('something went wrong!',err)
+// });
+
+// //Fetch only the changed data
+// db.ref().on('value',(snapshot)=>{
+//     const data = snapshot.val();
+//     console.log('data fetched successsfully :', data);
+//     });
+
+
+// db.ref().set({
+// artist: 'Sean Paul',
+// riddim : "Bubble Up",
+// year : 2005,
+// available : true,
+// stores : ['Apple Store','Google Play','Basecamp'],
+// released : {
+//     songname : 'legalize it',
+//     country: 'Jamaica',
+//     recorded : 2005,
+//     price : '$10.00'
+// }
+// }).then(console.log('Data is saved!')).catch((err)=>{
+// console.log("Opps...error! :", err)
+// })
+
+// //Change Firebase DB values
+// db.ref('released/price').set('$19.99').then(console.log('price updated!')).catch((e)=>{
+//     console.error('update price failed!', e)
+// });
+// db.ref('released/country').set('New Kingston,Jamaica').then(console.log('country updated!')).catch((err)=>{
+//     console.error('Country update failed!' ,err)
+// });
+
+
+//Remove DB Elements
+// var removeStores = db.ref('stores');
+// removeStores.remove()
+//     .then(function () {
+//         console.log("Remove succeeded.")
+//     })
+//     .catch(function (error) {
+//         console.log("Remove failed: " + error.message)
+//     });
+
+//Update the data 
+
+// db.ref().update({
+//     artist: 'Buju Banton',
+//     riddim: "Batty Rider",
+//     year: 1992,
+
+// });
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,29 +1,29 @@
-const promise = new Promise((resolve,reject)=>{
-  setTimeout(() => {
-  resolve({
-      producers: [{
-          id: 1,
-          name: 'Dave Kelly'
-                 }, 
-        {
-          id: 2,
-          name: 'Mad House'
-      }, {
-          id: 3,
-          name: 'Workie Workie'
-      }]
-  })
-reject("No data found");
-    }, 6000);
+// const promise = new Promise((resolve,reject)=>{
+//   setTimeout(() => {
+//   resolve({
+//       producers: [{
+//           id: 1,
+//           name: 'Dave Kelly'
+//                  }, 
+//         {
+//           id: 2,
+//           name: 'Mad House'
+//       }, {
+//           id: 3,
+//           name: 'Workie Workie'
+//       }]
+//   })
+// reject("No data found");
+//     }, 6000);
 
-});
-console.log("before===>");
+// });
+// console.log("before===>");
 
-promise.then((data)=>{
-    console.log(data.producers[0]);
-}).catch((error)=>{
-    console.log(error);
-})
+// promise.then((data)=>{
+//     console.log(data.producers[0]);
+// }).catch((error)=>{
+//     console.log(error);
+// })
 
 
-console.log("<====after");
+// console.log("<====after");
